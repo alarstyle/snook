369bb32715bbd83900bad9e6ac71e493b8a45d26
Alexander Stulnikov <alarstyle@gmail.com>
2013-01-19 00:00:41 +0200

    update

--- web.js
+++ web.js
@@ -1,9 +1,18 @@
 var express = require('express');
+var pg = require('pg');
+
+//pg.connect(process.env.DATABASE_URL, function(err, client) {
+  //var query = client.query('SELECT * FROM your_table');
+
+  //query.on('row', function(row) {
+    //console.log(JSON.stringify(row));
+  //});
+//});
 
 var app = express.createServer(express.logger());
 /*11*/
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send('Hello World! '+process.env.DATABASE_URL);
 });
 
 var port = process.env.PORT || 5000;
