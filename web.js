459982484d16dfdc038d339e7567b6e1730cf4ed
Alexander Stulnikov <alarstyle@gmail.com>
2013-01-19 11:44:40 +0200

    update

--- web.js
+++ web.js
@@ -12,11 +12,10 @@ var express = require('express'),
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(process.env.DATABASE_URL +"   ");
+  response.send(" HI ");
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log(process.env.DATABASE_URL);
   console.log("Listening on " + port);
 });
