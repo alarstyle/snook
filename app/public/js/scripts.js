feb9f32dad0be4bdee09830e6d4238975fce4594
alarstyle <alarstyle@gmail.com>
2013-09-08 12:21:15 +0300

    верстка страницы пользователя

--- app/public/js/scripts.js
+++ app/public/js/scripts.js
@@ -1,7 +1,17 @@
 (function($){
+    
+    function checkModernizr() {
+        //Modernizr.canvas
+        //Modernizr.draganddrop
+        //Modernizr.cssanimations
+        //Modernizr.csstransforms
+        //Modernizr.csstransforms3d
+        //Modernizr.csstransitions
+        //Modernizr.touch
+    }
 
 	$(document).ready(function() {
-        //PicasaLoader.useralbum('alarstyle', 'testalbum');
+        checkModernizr();
 	});
 
 	$(window).load(function(){
